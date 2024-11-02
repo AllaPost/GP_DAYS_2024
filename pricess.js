4a476870a8387961382e44e8320f94f1dd1bc49d
Alla Postnikova <allapostnikova09@gmail.com>
2024-11-02 22:49:30 +0300

    адаптив

--- pricess.js
+++ pricess.js
@@ -1,25 +1,51 @@
 // Инициализируем Swiper
-new Swiper('.image-slider' ,{
-// Cтрелки
-navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-},
-// Буллеты, текущее положение, прогрессбар
-pagination: {
-    el: '.swiper-pagination',
+new Swiper(".image-slider", {
+  // Cтрелки
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Буллеты, текущее положение, прогрессбар
+  pagination: {
+    el: ".swiper-pagination",
     // Буллеты
     clickable: true,
     // Динамические буллеты
-    dynamicBullets: true, 
-},
+    dynamicBullets: true,
+  },
 
-// Количество слайдеров для показа
-slidesPerView: 4.3,
+  // Количество слайдеров для показа
+  slidesPerView: 3.5,
 
-// Отступ между слайдерами
-spaceBetween: 30,
+  // Отступ между слайдерами
+  spaceBetween: 30,
 
-// Активный слайд
-
-}); 
+  centeredSlides: true,
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.1,
+    },
+    483: {
+      slidesPerView: 1.14,
+    },
+    517: {
+      slidesPerView: 1.2,
+    },
+    639: {
+      slidesPerView: 1.5,
+    },
+    783: {
+      slidesPerView: 1.8,
+    },
+    905: {
+      slidesPerView: 2.1,
+    },
+    1101: {
+      slidesPerView: 2.5,
+    },
+    1569: {
+      slidesPerView: 3.5,
+    },
+  },
+});
